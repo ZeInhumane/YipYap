7e817d7f7483fc11746f591752925feb0a382c27
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-18 21:13:26 +0800

    Update random.js

--- random.js
+++ random.js
@@ -1,4 +1,10 @@
 console.log("Matthew");
 console.log("gay");
 console.log("Matthew");
-console.log("test")
+console.log("test")class Hero {
+    constructor(HP, attack, speed){
+        this.HP = HP;
+        this.attack = attack;
+        this.speed = speed;
+    }
+}
