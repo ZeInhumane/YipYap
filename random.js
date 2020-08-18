f6aa8459c62c3c29e317f40a2ae2ce635ac796c6
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-18 21:41:02 +0800

    Update random.js

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 class Hero {
-    constructor(name, hp, attack, defense, speed){
+    constructor(name, hp, attack, defense, speed) {
         this.name = name;
         this.hp = hp;
         this.attack = attack;
@@ -7,13 +7,13 @@ class Hero {
         this.speed = speed;
     }
 
-    takeDamage(damage){
+    takeDamage(damage) {
         this.hp -= damage;
     }
 }
 
 class Enemy {
-    constructor(name, hp, attack, defense, speed, type){
+    constructor(name, hp, attack, defense, speed, type) {
         this.name = name;
         this.hp = hp;
         this.attack = attack;
@@ -22,8 +22,44 @@ class Enemy {
         this.type = type;
     }
 
-    takeDamage(damage){
+    takeDamage(damage) {
         this.hp -= damage;
+    }
+}
+
+function battle(player, enemy) {
+    function playerTurn() {
+        console.log(player.name + "'s turn!\n" + player.name + " does " + player.attack + " damage!\n");
+        enemy.takeDamage(player.attack);
+    }
+
+    function enemyTurn() {
+        console.log(enemy.name + "'s turn!\n" + enemy.name + " does " + enemy.attack + " damage!\n");
+        player.takeDamage(enemy.attack);
+    }
+
+    while (!(player.hp < 0) && !(enemy.hp < 0)) {
+        if (player.speed > enemy.speed) {
+            playerTurn();
+            if (enemy.hp > 0) {
+                enemyTurn();
+            }
+            console.log("\n");
+        }
+        else{
+            enemyTurn();
+            if(player.hp > 0){
+                playerTurn();
+            }
+            console.log("\n");
+        }
+    }
+
+    if(player.hp > 0){
+        console.log(player.name + " defeated " + enemy.name + "!");
+    }
+    else{
+        console.log(player.name + " has been defeated by " + enemy.name + "!");
     }
 }
 
@@ -31,6 +67,4 @@ var skeleton1 = new Enemy("Skele Boy", 10, 2, 1, 3, "undead");
 
 var matthew = new Hero("Matthew", 100, 10, 10, 15);
 
-matthew.takeDamage(skeleton1.attack);
-
-console.log(matthew.hp);
+battle(matthew, skeleton1);
