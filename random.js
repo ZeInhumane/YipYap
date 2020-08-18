66ce97749f91f1fc761f2e73ef8b73481720ff85
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-18 21:25:07 +0800

    Update random.js

--- random.js
+++ random.js
@@ -1,18 +1,21 @@
 class Hero {
-    constructor(HP, attack, defense, speed){
-        this.HP = HP;
+    constructor(name, hp, attack, defense, speed){
+        this.name = name;
+        this.hp = hp;
         this.attack = attack;
+        this.defense = defense;
         this.speed = speed;
     }
 
     takeDamage(damage){
-        this.HP -= damage;
+        this.hp -= damage;
     }
 }
 
 class Enemy {
-    constructor(HP, attack, defense, speed, type){
-        this.HP = HP;
+    constructor(name, hp, attack, defense, speed, type){
+        this.name = name;
+        this.hp = hp;
         this.attack = attack;
         this.defense = defense;
         this.speed = speed;
@@ -20,6 +23,14 @@ class Enemy {
     }
 
     takeDamage(damage){
-        this.HP -= damage;
+        this.hp -= damage;
     }
 }
+
+var skeleton1 = new Enemy("Skele Boy", 10, 2, 1, 3, "undead");
+
+var matthew = new Hero("Matthew", 100, 10, 10, 15);
+
+matthew.takeDamage(skeleton1.attack);
+
+console.log(matthew.hp);
