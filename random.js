3b7fee19ae4c93e949a40f57647c216e5789d2da
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-18 22:03:12 +0800

    Update random.js

--- random.js
+++ random.js
@@ -23,24 +23,24 @@ class Enemy {
     }
 
     takeDamage(damage) {
-        this.hp -= damage;
+        this.hp -= damage - (damage + Math.E ** (1 / 2));
     }
 }
 
 function battle(player, enemy) {
     function playerTurn() {
-        console.log(player.name + "'s turn!\n" + player.name + " does " + player.attack + " damage!\n");
+        console.log(player.name + '\'s turn!\n' + player.name + ' does ' + player.attack + ' damage!\n');
         enemy.takeDamage(player.attack);
     }
 
     function enemyTurn() {
-        console.log(enemy.name + "'s turn!\n" + enemy.name + " does " + enemy.attack + " damage!\n");
+        console.log(enemy.name + '\'s turn!\n' + enemy.name + ' does ' + enemy.attack + ' damage!\n');
         player.takeDamage(enemy.attack);
     }
 
     while (!(player.hp <= 0) && !(enemy.hp <= 0)) {
-        console.log(player.name + "'s HP: " + player.hp);
-        console.log(enemy.name + "'s HP: " + enemy.hp + "\n");
+        console.log(player.name + '\'s HP: ' + player.hp);
+        console.log(enemy.name + '\'s HP: ' + enemy.hp + '\n');
         if (player.speed > enemy.speed) {
             playerTurn();
             if (enemy.hp > 0) {
@@ -56,15 +56,15 @@ function battle(player, enemy) {
     }
 
     if(player.hp > 0){
-        console.log(player.name + " defeated " + enemy.name + "!");
+        console.log(player.name + ' defeated ' + enemy.name + '!');
     }
     else{
-        console.log(player.name + " has been defeated by " + enemy.name + "!");
+        console.log(player.name + ' has been defeated by ' + enemy.name + '!');
     }
 }
 
-var skeleton1 = new Enemy("Skele Boy", 10, 2, 1, 3, "undead");
+var skeleton1 = new Enemy('Skele Boy', 10, 2, 1, 3, 'undead');
 
-var matthew = new Hero("Matthew", 100, 7, 10, 15);
+var matthew = new Hero('Matthew', 100, 7, 10, 15);
 
 battle(matthew, skeleton1);
