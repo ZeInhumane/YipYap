a0eb50c53cc24663577bca4c27a595aea957b907
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-18 21:20:06 +0800

    Update random.js

--- random.js
+++ random.js
@@ -1,7 +1,25 @@
 class Hero {
-    constructor(HP, attack, speed){
+    constructor(HP, attack, defense, speed){
         this.HP = HP;
         this.attack = attack;
         this.speed = speed;
+    }
+
+    takeDamage(damage){
+        this.HP -= damage;
+    }
+}
+
+class Enemy {
+    constructor(HP, attack, defense, speed, type){
+        this.HP = HP;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.type = type;
+    }
+
+    takeDamage(damage){
+        this.HP -= damage;
     }
 }
