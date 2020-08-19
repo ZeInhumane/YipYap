c95d7bcb94199a8aa41bc37f35f181a344802700
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-19 20:31:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,17 +15,36 @@ client.on('message', message => {
         message.channel.send('Successful registration');
     }
     if (message.content === `${prefix}battle`) {
-        message.channel.send("Battle Start! :crossed_swords:")
+        const battleEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Battle Start! :crossed_swords:')
+            .setURL('https://discord.js.org/')
+            .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addField('Inline field title', 'Some value here', true)
+            .setImage('https://i.imgur.com/wSTFkRM.png')
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+        message.channel.send(battleEmbed)
             .then(botMessage => {
                 botMessage.react("⚔️");
                 botMessage.react("🛡️");
             })
             .then(() => {
                 const filter = (reaction, user) => (reaction.emoji.name === '⚔️' || reaction.emoji.name === '🛡️');
-                const collector = embedMsg.message.createReactionCollector(filter, { time: 10000 });
+                const collector = battleEmbed.message.createReactionCollector(filter, { time: 10000 });
                 collector.on('collect', r => r.emoji.name === '⚔️' ?
                     console.log('Reacted Yes') : console.log('Reacted No'));
             })
+
 
     }
 });
