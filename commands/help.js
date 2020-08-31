2cd50479b0bc0039bcf40ef1186d690f451d6eb4
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-08-31 11:07:09 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,16 +17,52 @@ module.exports = {
                     .addField('To get help for each command, do =help (command name)');
                 message.channel.send(helpEmbed);
                 break;
+            case "starthelp":
+                var startEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Batlle help')
+                    .addFields(
+                        { name: 'command1', value: "Battle is to fight against enemies to gain gold and exp" },
+                        { name: 'command2', value: "nothing" },
+                        { name: 'command3', value: "nothing" }
+                    )
+                    .addField('To get help for each command, do =help (command name)');
+                message.channel.send(startEmbed);
+                break;
+            case "battlehelp":
+                var battleEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Batlle help')
+                    .addFields(
+                        { name: 'command1', value: "Battle is to fight against enemies to gain gold and exp" },
+                        { name: 'command2', value: "nothing" },
+                        { name: 'command3', value: "nothing" }
+                    )
+                    .addField('To get help for each command, do =help (command name)');
+                message.channel.send(battleEmbed);
+                break;
+            case "gayhelp":
+                var gayEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Batlle help')
+                    .addFields(
+                        { name: 'command1', value: "Battle is to fight against enemies to gain gold and exp" },
+                        { name: 'command2', value: "nothing" },
+                        { name: 'command3', value: "nothing" }
+                    )
+                    .addField('To get help for each command, do =help (command name)');
+                message.channel.send(gayEmbed);
+                break;
             default:
-                var helpEmbed = new Discord.MessageEmbed()
+                helpEmbed = new Discord.MessageEmbed()
                     .setColor('#0099ff')
                     .setTitle('All commands')
                     .addFields(
                         { name: 'command1', value: "battle" },
                         { name: 'command2', value: "start" },
-                        { name: 'command3', value: "nothing yet" }
-                    )
-                    .addField('To get help for each command, do =help (command name)');
+                        { name: 'command3', value: "nothing yet" },
+                        { name: 'To get help for each command do =help {command name}', value: "" },
+                    );
                 message.channel.send(helpEmbed);
         }
     },
