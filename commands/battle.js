f85fa5e839569a5f7fe8cb01bd59c7964c92c5e6
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-08-31 14:48:11 +0800

    Update battle.js

--- commands/battle.js
+++ commands/battle.js
@@ -2,194 +2,191 @@ module.exports = {
     name: "battle",
     description: "Battles an enemy",
     execute(message, args) {
-        async function main() {
-            const Discord = require('discord.js');
-            class Hero {
-                constructor(name, hp, attack, defense, speed) {
-                    this.name = name;
-                    this.hp = hp;
-                    this.attack = attack;
-                    this.defense = defense;
-                    this.speed = speed;
-                }
-
-                takeDamage(damage) {
-                    let attMulti = damage / this.defense;
-                    if (attMulti < 0.4) {
-                        attMulti = 0.4;
-                    }
-                    else if (attMulti > 1.5) {
-                        attMulti = 1.5;
-                    }
-                    var damageTaken = Math.floor((damage + Math.floor((damage - this.defense) / 4)) * attMulti);
-                    if (damageTaken < 1) {
-                        damageTaken = 1;
-                    }
-                    this.hp -= damageTaken;
-                    return damageTaken;
-                }
+        const Discord = require('discord.js');
+        class Hero {
+            constructor(name, hp, attack, defense, speed) {
+                this.name = name;
+                this.hp = hp;
+                this.attack = attack;
+                this.defense = defense;
+                this.speed = speed;
             }
 
-            class Enemy {
-                constructor(name, hp, attack, defense, speed, type) {
-                    this.name = name;
-                    this.hp = hp;
-                    this.attack = attack;
-                    this.defense = defense;
-                    this.speed = speed;
-                    this.type = type;
+            takeDamage(damage) {
+                let attMulti = damage / this.defense;
+                if (attMulti < 0.4) {
+                    attMulti = 0.4;
                 }
+                else if (attMulti > 1.5) {
+                    attMulti = 1.5;
+                }
+                var damageTaken = Math.floor((damage + Math.floor((damage - this.defense) / 4)) * attMulti);
+                if (damageTaken < 1) {
+                    damageTaken = 1;
+                }
+                this.hp -= damageTaken;
+                return damageTaken;
+            }
+        }
 
-                takeDamage(damage) {
-                    let attMulti = damage / this.defense;
-                    if (attMulti < 0.4) {
-                        attMulti = 0.4;
-                    }
-                    else if (attMulti > 1.5) {
-                        attMulti = 1.5;
-                    }
-                    var damageTaken = Math.floor((damage + Math.floor((damage - this.defense) / 4)) * attMulti);
-                    if (damageTaken < 1) {
-                        damageTaken = 1;
-                    }
-                    this.hp -= damageTaken;
-                    return damageTaken;
-                }
+        class Enemy {
+            constructor(name, hp, attack, defense, speed, type) {
+                this.name = name;
+                this.hp = hp;
+                this.attack = attack;
+                this.defense = defense;
+                this.speed = speed;
+                this.type = type;
             }
 
-            async function battle(player, enemy) {
-                function playerTurn(action) {
-                    if (action == "⚔️") {
-                        playerTurnAction = player.name + '\'s turn!\n' + player.name + ' does ' + enemy.takeDamage(player.attack) + ' damage!\n';
-                    }
-                    else if (action == "🛡️"){
-                        playerTurnAction = "You shield yourself, it didn't work";
-                    }
-                    else {
-                        playerTurnAction = "Nothing happened";
-                    }
+            takeDamage(damage) {
+                let attMulti = damage / this.defense;
+                if (attMulti < 0.4) {
+                    attMulti = 0.4;
+                }
+                else if (attMulti > 1.5) {
+                    attMulti = 1.5;
+                }
+                var damageTaken = Math.floor((damage + Math.floor((damage - this.defense) / 4)) * attMulti);
+                if (damageTaken < 1) {
+                    damageTaken = 1;
+                }
+                this.hp -= damageTaken;
+                return damageTaken;
+            }
+        }
 
+        async function battle(player, enemy) {
+            function playerTurn(action) {
+                if (action == "⚔️") {
+                    playerTurnAction = player.name + '\'s turn!\n' + player.name + ' does ' + enemy.takeDamage(player.attack) + ' damage!\n';
                 }
-
-                function enemyTurn() {
-                    enemyTurnAction = enemy.name + '\'s turn!\n' + enemy.name + ' does ' + player.takeDamage(enemy.attack) + ' damage!\n';
-                }
-                function createUpdatedMessage() {
-                    var updatedBattleEmbed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setTitle('Battle Start! :crossed_swords:')
-                        .setURL('https://discord.gg/CTMTtQV')
-                        .setAuthor('Inhumane', 'https://vignette.wikia.nocookie.net/hunter-x-hunter-fanon/images/a/a9/BABC6A23-98EF-498E-9D0E-3EBFC7ED8626.jpeg/revision/latest?cb=20170930221652', 'https://discord.js.org')
-                        .setDescription('Absolute best')
-                        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-                        .addFields(
-                            { name: 'Player HP', value: player.name + '\'s HP: ' + player.hp },
-                            { name: 'Enemy HP', value: enemy.name + '\'s HP: ' + enemy.hp },
-                            { name: '\u200B', value: '\u200B' },
-                            { name: 'Turn', value: playerTurnAction },
-                            { name: '​', value: enemyTurnAction }
-                        )
-                        .addField('Bloody battlefield', '10% Less speed debuff', true)
-                        .setImage('https://tinyurl.com/y4yl2xaa')
-                        .setTimestamp()
-                        .setFooter('Fight', 'https://tinyurl.com/y4yl2xaa');
-                    return updatedBattleEmbed;
-                }
-                while (!(player.hp <= 0) && !(enemy.hp <= 0)) {
-                    console.log(player.hp, enemy.hp)
-                    var turn, playerAction, playerTurnAction, enemyTurnAction;
-                    await new Promise((resolve, reject) => {
-                        const collector = botEmbedMessage.createReactionCollector(filter, { max: 1, time: 60000 });
-                        collector.on('collect', r => {
-                            collector.time = 60000;
-                            console.log(r.emoji.name);
-                            playerAction = r.emoji.name
-                            resolve();
-                            
-                        });
-                    });
-                    console.log("BATTLE STARTS");
-                    console.log(playerAction);
-                    if (player.speed > enemy.speed) {
-                        playerTurn(playerAction);
-                        if (enemy.hp > 0) {
-                            enemyTurn();
-                        }
-                    }
-                    else {
-                        enemyTurn();
-                        if (player.hp > 0) {
-                            playerTurn(playerAction);
-                        }
-                    }
-                    botEmbedMessage.edit(createUpdatedMessage());
-                }
-
-                if (player.hp > 0) {
-                    message.channel.send(player.name + ' defeated ' + enemy.name + '!');
+                else if (action == "🛡️") {
+                    playerTurnAction = "You shield yourself, it didn't work";
                 }
                 else {
-                    message.channel.send(player.name + ' has been defeated by ' + enemy.name + '!');
+                    playerTurnAction = "Nothing happened";
                 }
+
             }
 
-            function makeNewEnemy() {
-                var enemyHP = Math.floor(Math.random() * 51 + 10);
-                var enemyAttack = Math.floor(Math.random() * 11);
-                var enemyDefense = Math.floor(Math.random() * 11);
-                var enemySpeed = Math.floor(Math.random() * 50 + 10);
-                var enemyType = "undead";
-                var enemy = new Enemy("Skele Man", enemyHP, enemyAttack, enemyDefense, enemySpeed, enemyType);
-                return enemy;
+            function enemyTurn() {
+                enemyTurnAction = enemy.name + '\'s turn!\n' + enemy.name + ' does ' + player.takeDamage(enemy.attack) + ' damage!\n';
             }
-
-            function makeNewPlayer(playerName) {
-                playerDataBase.push(new Hero(playerName, 50, 5, 5, 5));
+            function createUpdatedMessage() {
+                var updatedBattleEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Battle Start! :crossed_swords:')
+                    .setURL('https://discord.gg/CTMTtQV')
+                    .setAuthor('Inhumane', 'https://vignette.wikia.nocookie.net/hunter-x-hunter-fanon/images/a/a9/BABC6A23-98EF-498E-9D0E-3EBFC7ED8626.jpeg/revision/latest?cb=20170930221652', 'https://discord.js.org')
+                    .setDescription('Absolute best')
+                    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+                    .addFields(
+                        { name: 'Player HP', value: player.name + '\'s HP: ' + player.hp },
+                        { name: 'Enemy HP', value: enemy.name + '\'s HP: ' + enemy.hp },
+                        { name: '\u200B', value: '\u200B' },
+                        { name: 'Turn', value: playerTurnAction },
+                        { name: '​', value: enemyTurnAction }
+                    )
+                    .addField('Bloody battlefield', '10% Less speed debuff', true)
+                    .setImage('https://tinyurl.com/y4yl2xaa')
+                    .setTimestamp()
+                    .setFooter('Fight', 'https://tinyurl.com/y4yl2xaa');
+                return updatedBattleEmbed;
             }
+            while (!(player.hp <= 0) && !(enemy.hp <= 0)) {
+                console.log(player.hp, enemy.hp)
+                var turn, playerAction, playerTurnAction, enemyTurnAction;
+                await new Promise((resolve, reject) => {
+                    const collector = botEmbedMessage.createReactionCollector(filter, { max: 1, time: 60000 });
+                    collector.on('collect', r => {
+                        collector.time = 60000;
+                        console.log(r.emoji.name);
+                        playerAction = r.emoji.name
+                        resolve();
 
-            var playerDataBase = [];
-            var matthew = new Hero('Matthew', 100, 7, 10, 15);
-            var enemy = makeNewEnemy();
-            console.log(message.author.id);
-            const filter = (reaction, user) => {
-                console.log("Check " + reaction.emoji.name);
-                if ((reaction.emoji.name === '⚔️' || reaction.emoji.name === '🛡️') && user == message.author.id) {
-                    console.log(reaction.emoji.name + " passed");
-                    return reaction;
+                    });
+                });
+                console.log("BATTLE STARTS");
+                console.log(playerAction);
+                if (player.speed > enemy.speed) {
+                    playerTurn(playerAction);
+                    if (enemy.hp > 0) {
+                        enemyTurn();
+                    }
                 }
-            };
+                else {
+                    enemyTurn();
+                    if (player.hp > 0) {
+                        playerTurn(playerAction);
+                    }
+                }
+                botEmbedMessage.edit(createUpdatedMessage());
+            }
 
-            const battleEmbed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Battle Start! :crossed_swords:')
-                .setURL('https://discord.gg/CTMTtQV')
-                .setAuthor('Inhumane', 'https://vignette.wikia.nocookie.net/hunter-x-hunter-fanon/images/a/a9/BABC6A23-98EF-498E-9D0E-3EBFC7ED8626.jpeg/revision/latest?cb=20170930221652', 'https://discord.js.org')
-                .setDescription('Absolute best')
-                .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-                .addFields(
-                    { name: 'Player HP', value: matthew.name + '\'s HP: ' + matthew.hp },
-                    { name: 'Enemy HP', value: enemy.name + '\'s HP: ' + enemy.hp },
-                    { name: '\u200B', value: '\u200B' }
-                )
-                .addField('Bloody battlefield', '10% Less speed debuff', true)
-                .setImage('https://tinyurl.com/y4yl2xaa')
-                .setTimestamp()
-                .setFooter('Fight', 'https://tinyurl.com/y4yl2xaa');
-
-
-            var botEmbedMessage;
-            var collector;
-            message.channel.send(battleEmbed)
-                .then(botMessage => {
-                    botEmbedMessage = botMessage;
-                    botMessage.react("⚔️");
-                    botMessage.react("🛡️");
-
-                    // collector = botMessage.createReactionCollector(filter, { max: 1, time: 60000 });
-                    // Replace matthew with the message author
-                    battle(matthew, enemy);
-                })
+            if (player.hp > 0) {
+                message.channel.send(player.name + ' defeated ' + enemy.name + '!');
+            }
+            else {
+                message.channel.send(player.name + ' has been defeated by ' + enemy.name + '!');
+            }
         }
-        main();
+
+        function makeNewEnemy() {
+            var enemyHP = Math.floor(Math.random() * 51 + 10);
+            var enemyAttack = Math.floor(Math.random() * 11);
+            var enemyDefense = Math.floor(Math.random() * 11);
+            var enemySpeed = Math.floor(Math.random() * 50 + 10);
+            var enemyType = "undead";
+            var enemy = new Enemy("Skele Man", enemyHP, enemyAttack, enemyDefense, enemySpeed, enemyType);
+            return enemy;
+        }
+
+        function makeNewPlayer(playerName) {
+            playerDataBase.push(new Hero(playerName, 50, 5, 5, 5));
+        }
+
+        var playerDataBase = [];
+        var matthew = new Hero('Matthew', 100, 7, 10, 15);
+        var enemy = makeNewEnemy();
+        console.log(message.author.id);
+        const filter = (reaction, user) => {
+            console.log("Check " + reaction.emoji.name);
+            if ((reaction.emoji.name === '⚔️' || reaction.emoji.name === '🛡️') && user == message.author.id) {
+                console.log(reaction.emoji.name + " passed");
+                return reaction;
+            }
+        };
+
+        const battleEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Battle Start! :crossed_swords:')
+            .setURL('https://discord.gg/CTMTtQV')
+            .setAuthor('Inhumane', 'https://vignette.wikia.nocookie.net/hunter-x-hunter-fanon/images/a/a9/BABC6A23-98EF-498E-9D0E-3EBFC7ED8626.jpeg/revision/latest?cb=20170930221652', 'https://discord.js.org')
+            .setDescription('Absolute best')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .addFields(
+                { name: 'Player HP', value: matthew.name + '\'s HP: ' + matthew.hp },
+                { name: 'Enemy HP', value: enemy.name + '\'s HP: ' + enemy.hp },
+                { name: '\u200B', value: '\u200B' }
+            )
+            .addField('Bloody battlefield', '10% Less speed debuff', true)
+            .setImage('https://tinyurl.com/y4yl2xaa')
+            .setTimestamp()
+            .setFooter('Fight', 'https://tinyurl.com/y4yl2xaa');
+
+
+        var botEmbedMessage;
+        var collector;
+        message.channel.send(battleEmbed)
+            .then(botMessage => {
+                botEmbedMessage = botMessage;
+                botMessage.react("⚔️");
+                botMessage.react("🛡️");
+
+                // collector = botMessage.createReactionCollector(filter, { max: 1, time: 60000 });
+                // Replace matthew with the message author
+                battle(matthew, enemy);
+            });
     }
 }
