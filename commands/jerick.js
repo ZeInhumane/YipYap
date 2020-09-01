efde0aa818c9f8637af95987e8670a0d31140d5c
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-01 20:07:29 +0800

    Update jerick.js

--- commands/jerick.js
+++ commands/jerick.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "jerick",
     description: "Pings Jerick",
     execute(message, args) {
-        message.channel.send('@jerick5555#1831 is the best');
+        message.channel.send('<@jerick5555#1831> is the best');
     }
 }
