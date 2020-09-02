7d76a259074335dc7b6be719549f540e882c7809
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-02 12:55:08 +0800

    Update jerick.js

--- commands/jerick.js
+++ commands/jerick.js
@@ -2,9 +2,15 @@ module.exports = {
     name: "jerick",
     description: "Pings Jerick",
     execute(message, args) {
-        setInterval(botStatus, 1200);
-        function botStatus() {
-           message.channel.send('<@692612058860224543> is the best');
+        function pingPerson() {
+            for (i < 0; i < args[0]; i++) {
+                setInterval(pingPersonOnce, args[1]);
+                function pingPersonOnce() {
+                    message.channel.send('<@692612058860224543> is the best');
+                    clearInterval()
+                }
+            }
         }
+        pingPerson();
     },
 };
