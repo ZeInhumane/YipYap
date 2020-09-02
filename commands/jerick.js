3b19a8b59b53f5236b5dbc801291a67e3dfe9c92
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-02 14:38:29 +0800

    Update jerick.js

--- commands/jerick.js
+++ commands/jerick.js
@@ -2,19 +2,17 @@ module.exports = {
     name: "jerick",
     description: "Pings Jerick",
     execute(message, args) {
-        var i;
         args[1] = parseInt(args[1]);
-        var increment = parseInt(args[1]);
-        function pingPerson() {
-            args[1] = parseInt(increment) + args[1];
-            setTimeout(pingPersonOnce, args[1]);
+        function pingPerson(i) {
+            args[1] *= i
+            setTimeout(pingPersonOnce, parseInt(args[1]));
             console.log(args[1]);
         }
         function pingPersonOnce() {
             message.channel.send('<@692612058860224543> is the best');
         }
-        for (i = 0; i < args[0]; i++) {
-            pingPerson();
+        for (var i = 1; i <= args[0]; i++) {
+            pingPerson(i);
         }
     },
 };
