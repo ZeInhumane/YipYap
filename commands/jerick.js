2290e32ca9f53716397f35d89c13dcd749839837
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-02 14:21:33 +0800

    Update jerick.js

--- commands/jerick.js
+++ commands/jerick.js
@@ -3,8 +3,8 @@ module.exports = {
     description: "Pings Jerick",
     execute(message, args) {
         var i;
+        var increment = args[1];
         function pingPerson() {
-            var increment = args[1];
             args[1] += parseInt(increment);
             setTimeout(pingPersonOnce, args[1]);
             console.log(args[1]);
