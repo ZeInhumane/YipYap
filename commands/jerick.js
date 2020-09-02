a48747f2f2df902311a26d0a11a5a18a59e3eef2
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-02 14:06:55 +0800

    Update jerick.js

--- commands/jerick.js
+++ commands/jerick.js
@@ -4,14 +4,14 @@ module.exports = {
     execute(message, args) {
         var i;
         function pingPerson() {
-                setTimeout(pingPersonOnce, args[1]);
-                var increment = args[1];
-                args[1] += increment;
+            var increment = args[1];
+            args[1] += increment;
+            setTimeout(pingPersonOnce, args[1]);
         }
         function pingPersonOnce() {
             message.channel.send('<@692612058860224543> is the best');
         }
-        for(i = -1; i < args[0]; i++){
+        for (i = -1; i < args[0]; i++) {
             pingPerson();
         }
     },
