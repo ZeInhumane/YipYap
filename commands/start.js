256531b0904210df7ecc887d708f8c2f43c5bb08
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-03 20:05:15 +0800

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -15,7 +15,7 @@ module.exports = {
         .then(result => console.log(result))
         .catch(err => console.error(err));
         console.log('user!' + message.author.id + 'registered');
-        var localCurrency = db.user.findOne({ 'userID' : message.author.id }, { 'currency':1, _id:0 });
+        var localCurrency = user.userSchema.findOne({ 'userID' : message.author.id }, { 'currency':1, _id:0 });
         console.log(localCurrency);
         message.channel.send('Successful registration you start with' + localCurrency);
     },
