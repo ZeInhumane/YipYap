eb5e600bd3980af37c7440cb65c57c42aedcb251
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-03 22:39:17 +0800

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -7,7 +7,7 @@ module.exports = {
     description: "Sets up a new player",
     execute(message, args) {
         user = User.findOne({ "userID": message.author.id });
-        console.log("This is the user " + user.get("userID"));
+        console.log("This is the user " + user);
         //if (err) {
             user = new User({
                 _id: mongoose.Types.ObjectId(),
