f4f58078e9f281cd00d57aefca1f3182d89fe5a5
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-03 20:31:05 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userID: String,
-    currency: String,
+    currency: Number,
 });
 
 module.exports = mongoose.model('User', userSchema, 'users');
