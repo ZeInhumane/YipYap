632b2e121b5949c367a5266aff85947d6fdc5f5e
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-04 11:22:31 +0800

    added player to userSchema

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userID: String,
     currency: Number,
+    player: Object,
 });
 
 module.exports = mongoose.model('User', userSchema, 'users');
