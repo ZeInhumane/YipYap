e954d2e294dfda1796b422a001ba00e7e948186b
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-04 10:47:28 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'pings',
+    name: 'ping',
     category: 'info',
     description: 'Returns bot and API latency in milliseconds.',
     usage: `ping`,
