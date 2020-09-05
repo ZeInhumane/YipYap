be87c483e5af0ab779f63b21e6ad95af95a4f9fb
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-05 21:29:00 +0800

    fixed reminder

--- commands/reminder.js
+++ commands/reminder.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "reminder",
-    description: "Pings someone a set number of times every set interval",
+    description: "Pings someone a set number of times every set interval\nSyntax: =reminder [Num ping] [Inteval(ms)] [Persona's ID]",
     execute(message, args) {
         function pingPersonOnce() {
             console.log(args[2])
@@ -15,26 +15,28 @@ module.exports = {
         if (message.member.roles.cache.some(role => role.name === 'cat') || message.member.hasPermission(['KICK_MEMBERS']) || message.member.hasPermission(['BAN_MEMBERS'])) {
             var timer = parseInt(args[1]);
             //Check for vaild arguments
-            if(isNaN(args[0]) || isNaN(args[1]) || isNaN(args[2])){
+            if (isNaN(args[0]) || isNaN(args[1]) || isNaN(args[2])) {
                 message.channel.send('Invaild arguments, all arguments must be a number');
             }
-            if(timer < 1000){
-                message.channel.send('Maximum message send speed is 1 per second');
-                timer = 1000;
-            }
-            else if(timer > 86400000){
-                message.channel.send('Minimum message send speed is 1 per day');
-                timer = 86400000;
-            }
-            if(args[0] > 100){
-                message.channel.send('Maximum messages able to be sent is 100');
-                args[0] = '100';
-            }
+            else {
+                if (timer < 1000) {
+                    message.channel.send('Maximum message send speed is 1 per second');
+                    timer = 1000;
+                }
+                else if (timer > 86400000) {
+                    message.channel.send('Minimum message send speed is 1 per day');
+                    timer = 86400000;
+                }
+                if (args[0] > 100) {
+                    message.channel.send('Maximum messages able to be sent is 100');
+                    args[0] = '100';
+                }
 
-            //Sends messages after timer
-            for (var i = 1; i <= parseInt(args[0]); i++) {
-                var messageTimer = timer * i
-                setTimeout(pingPersonOnce, messageTimer);
+                //Sends messages after timer
+                for (var i = 1; i <= parseInt(args[0]); i++) {
+                    var messageTimer = timer * i
+                    setTimeout(pingPersonOnce, messageTimer);
+                }
             }
         }
         else {
