825f2af2e30b4abd16b1e3ca0d58d22c4ace9060
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-05 20:47:34 +0800

    redo

--- commands/daily.js
+++ commands/daily.js
@@ -18,7 +18,7 @@ module.exports = {
                 user.save()
                     .then(result => console.log(result))
                     .catch(err => console.error(err));
-                message.channel.send('You have successfully claimed your daily of ' + 5 + ":de_money:");
+                message.channel.send('You have successfully claimed your daily of ' + 5 + "<a:cash_24:751784973488357457>");
                 message.channel.send("This database is gonna be purged so there really is no point in spamming daily, abby")
             }
         });
