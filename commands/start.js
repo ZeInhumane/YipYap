85c4781f7165df8b0db013cd05d3812d61054686
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-05 16:33:18 +0800

    revamped takeDamage function and a bug fix

--- commands/start.js
+++ commands/start.js
@@ -15,29 +15,6 @@ module.exports = {
                 this.defense = defense;
                 this.speed = speed;
             }
-            // Method to check for damage taken by hero
-            takeDamage(damage) {
-                let attMulti = damage / this.defense;
-                if (attMulti < 0.4) {
-                    attMulti = 0.4;
-                }
-                else if (attMulti > 1.5) {
-                    attMulti = 1.5;
-                }
-                var damageTaken = Math.floor((damage + Math.floor((damage - this.defense) / 4)) * attMulti);
-                console.log(playerAction);
-                if (playerAction == "🛡️") {
-                    // Change it later so higher level reduces damagetaken too
-                    damageTaken *= (100 - this.defense) / 100;
-                }
-                // Ensures damage taken is not negative
-                if (damageTaken < 0) {
-                    damageTaken = 0;
-                }
-                damageTaken = Math.floor(damageTaken);
-                this.hp -= damageTaken;
-                return damageTaken;
-            }
         }
         User.findOne({ userID: message.author.id }, (err, user) => {
             if (err) console.log(err);
@@ -46,7 +23,7 @@ module.exports = {
                     _id: mongoose.Types.ObjectId(),
                     userID: message.author.id,
                     currency: 0,
-                    player: new Hero((m = message.member.user.tag.toString()) => {return m.split("#", m.length - 4)[0]} , 50, 5, 5, 5)
+                    player: new Hero((m = message.member.user.tag.toString()) => {m = m.split("#", m.length - 4); return m[0]} , 50, 5, 5, 5)
                 });
                 user.save()
                     .then(result => console.log(result))
