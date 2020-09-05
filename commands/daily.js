d66d51247223fcca83a78afa54f4ae7023963472
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-05 21:10:50 +0800

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -14,12 +14,12 @@ module.exports = {
             }
             else {
                 user.currency += 5;
-                message.channel.send(user.currency);
                 user.save()
                     .then(result => console.log(result))
                     .catch(err => console.error(err));
                 message.channel.send('You have successfully claimed your daily of ' + 5 + "<:cash_24:751784973488357457>​");
                 message.channel.send("This database is gonna be purged so there really is no point in spamming daily, abby")
+                message.channel.send('you now have' + user.currency + '<:cash_24:751784973488357457>');
             }
         });
     }
