f80ad381d2b880686123a5d4419877aae7c183bf
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-06 14:12:45 +0800

    Made help dynamic

--- commands/help.js
+++ commands/help.js
@@ -5,38 +5,30 @@ module.exports = {
         const Discord = require('discord.js');
         const client = require('../index.js').client;
         message.channel.send('type help + {command name for specific help on that command}');
-        switch (args[0]) {
-            case "battle":
-            case "start":
-            case "ping":
-            case "highlight":
-            case "reminder":
-            case "daily":
-            case "currency":
-                var helpEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF69B4')
-                    .setTitle(client.commands.get(args[0]).name.charAt(0).toUpperCase() + client.commands.get(args[0]).name.slice(1) + ' help')
-                    .addFields(
-                        { name: client.commands.get(args[0]).description, value: "​" },
-                    );
-                message.channel.send(helpEmbed);
-                break;
-            //Description of start
-            //'Start registers you to the bot database to allow you to play the bot'
-            default:
-                var helpEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF69B4')
-                    .setTitle('All commands, To get help for each command do =help {command name}')
-                    .addFields(
-                        { name: 'start', value: "​" },
-                        { name: 'battle', value: "​" },
-                        { name: 'highlight', value: "​" },
-                        { name: 'ping', value: "​" },
-                        { name: 'reminder', value: "​" },
-                        { name: 'daily', value: "​" },
-                        { name: 'currency', value: "​" },
-                    );
-                message.channel.send(helpEmbed);
+        const command = client.commands.get(args[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(args[0]));
+        if (!command) {
+            var helpEmbed = new Discord.MessageEmbed()
+                .setColor('#FF69B4')
+                .setTitle('All commands, To get help for each command do =help {command name}')
+                .addFields(
+                    { name: 'start', value: "​" },
+                    { name: 'battle', value: "​" },
+                    { name: 'highlight', value: "​" },
+                    { name: 'ping', value: "​" },
+                    { name: 'reminder', value: "​" },
+                    { name: 'daily', value: "​" },
+                    { name: 'currency', value: "​" },
+                );
+            message.channel.send(helpEmbed);
         }
-    },
+        else {
+            var helpEmbed = new Discord.MessageEmbed()
+                .setColor('#FF69B4')
+                .setTitle(client.commands.get(args[0]).name.charAt(0).toUpperCase() + client.commands.get(args[0]).name.slice(1) + ' help')
+                .addFields(
+                    { name: client.commands.get(args[0]).description, value: "​" },
+                );
+            message.channel.send(helpEmbed);
+        }
+    }
 };
