5805c1c39762fcb114cee339248d5d9a4032a0ba
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-06 18:59:20 +0800

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -8,19 +8,46 @@ module.exports = {
     cooldown: 5,
     aliases: ['transfer'],
     execute(message, args) {
-                const currentAmount = user.currency(message.author.id);
-                const transferAmount = commandArgs.split(/ +/g).find(arg => !/<@!?\d+>/g.test(arg));
-                const transferTarget = message.mentions.users.first();
+        const currentAmount = User.findOne({ userID: message.author.id }, (err, user) => {
+            if (user == null) {
+                message.channel.send("You have not set up a player yet! Do =start to start.");
+            }
+            else {
+                user.currency;
+            }
+
+            const transferAmount = commandArgs.split(/ +/g).find(arg => !/<@!?\d+>/g.test(arg));
+
+            const transferTarget = User.findOne({ userID: message.mentions.users.first().id }, (err, user) => {
+                if (user == null) {
+                    message.channel.send("You have not set up a player yet! Do =start to start.");
+                }
+                else {
+                    user.currency;
+                }
 
                 if (!transferAmount || isNaN(transferAmount)) return message.channel.send(`Sorry ${message.author}, that's an invalid amount.`);
                 if (transferAmount > currentAmount) return message.channel.send(`Sorry ${message.author}, you only have ${currentAmount}.`);
                 if (transferAmount <= 0) return message.channel.send(`Please enter an amount greater than zero, ${message.author}.`);
 
-                user.currency.add(message.author.id, -transferAmount);
-                user.currency.add(transferTarget.id, transferAmount);
+                const currentAmount = User.findOne({ userID: message.author.id }, (err, user) => {
+                    if (user == null) {
+                        message.channel.send("You have not set up a player yet! Do =start to start.");
+                    }
+                    else {
+                        user.currency -= transferAmount;
+                    }
 
-                return message.channel.send(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${user.currency(message.author.id)}💰`);
-            }
+                    const transferTarget = User.findOne({ userID: message.mentions.users.first().id }, (err, user) => {
+                        if (user == null) {
+                            message.channel.send("You have not set up a player yet! Do =start to start.");
+                        }
+                        else {
+                            user.currency += transferAmount;
+                        }
+
+                        return message.channel.send(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${user.currency}💰`);
+                    }
         });
-    }
+            }
 }
