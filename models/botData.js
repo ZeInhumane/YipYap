51548c5138ce59aa2d01ffe93742e2f438d0c39b
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-07 16:11:12 +0800

    Update botData.js

--- models/botData.js
+++ models/botData.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dataSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     dataName: String,
-    data: Object,
+    data: mongoose.Collection,
 });
 
 module.exports = mongoose.model('BotData', dataSchema, 'botData');
