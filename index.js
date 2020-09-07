db84d4baca4e3bf1c18f635796fa1be6fc3b4d69
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-07 15:00:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const BotData = require('./models/botData');
 
 client.mongoose = require('./utils/mongoose');
 client.commands = new Discord.Collection();
-const cooldowns = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
