d97eda0410a7653ec425db4c5dee537b3e2d99e6
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-07 17:18:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ client.once('ready', () => {
                 dataName: 'Cooldowns',
                 data: new Discord.Collection(),
             })
-            Data.markModified("data", '_id', 'dataName');
             Data.save()
                 .then(result => console.log(result))
                 .catch(err => console.error(err));
