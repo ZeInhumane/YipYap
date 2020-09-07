410a3bfebaf3c513aeddadadcb18a2074a165387
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-07 14:04:36 +0800

    Update win.js

--- commands/battle/win.js
+++ commands/battle/win.js
@@ -5,6 +5,9 @@ module.exports = {
         var moneyEarned = 1;
         User.findOne({ userID: message.author.id }, (err, user) => {
             winner.currency += moneyEarned;
+            winner.save()
+                    .then(result => console.log(result))
+                    .catch(err => console.error(err));
         });
         message.channel.send(winner.name + " got " + moneyEarned + "<:cash_24:751784973488357457>");
     }
