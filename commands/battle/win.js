91fce5bb1f5dc63d70d6228df03cdcd06093315f
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-08 12:15:13 +0800

    Update win.js

--- commands/battle/win.js
+++ commands/battle/win.js
@@ -2,6 +2,7 @@ module.exports = {
     execute(message, winner, loser) {
         const User = require('../../models/user');
         message.channel.send(winner.player.name + ' defeated ' + loser.name || loser.player.name + '!');
+        console.log(loser.name || loser.player.name);
         var moneyEarned = 1;
         User.findOne({ userID: winner.userID }, (err, user) => {
             user.currency += moneyEarned;
