15faa86910e4a27e0accb2a8096b3b915e0c95e1
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-09 15:26:53 +0800

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -18,12 +18,12 @@ module.exports = {
                     .exec(function (err, user) {
                         var lb = "Leaderboard for currency \n";
                         console.log("it is entering the before the for loop");
-                        if(args[1] < 0 || args[1] > 3 || args[1] == null) {
-                            args[1] = 3;
+                        if(args[1] < 0 || args[1] > 9 || args[1] == null) {
+                            args[1] = 9;
                         }
                         for (var i = 0; i < args[1]; i++) {
                             console.log(user[i].userID);
-                            lb += "\n" + client.users.cache.get(user[i].userID).tag + "\n " + user[i].currency + "<:cash_24:751784973488357457>";
+                            lb += "\n" + client.users.cache.get(user[i].userID).tag + "\n " + user[i].currency + " <:cash_24:751784973488357457>";
                             console.log("user[i}.currency" + user[i].currency);
                             console.log("it is entering the for loop");
                         }
