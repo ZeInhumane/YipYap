1c5000f5750a6028558de3fdeb2711b892a9b75e
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-09 15:23:53 +0800

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,16 +3,35 @@ const mongoose = require('mongoose');
 const Discord = require('discord.js');
 
 module.exports = {
-    name: "a",
-    description: "Checks  leaderboard",
+    name: "leaderboard",
+    description: "Checks leaderboard",
     cooldown: 10,
     aliases: ['top'],
     execute(message, args) {
-        User.find({}, "currency userID", function(err, user) {
-            if (err) console.log(err);
-            console.log(user);
-            })
-            .sort([["currency", 1], ["userID", "descending"]]);
-            message.channel.send(user.currency);
+        const client = require('../index.js').client;
+        // Find First 10 News Items
+        switch (args[0]) {
+            case "currency":
+                console.log("it is entering the case");
+                User.find({})
+                    .sort({ currency: -1 })
+                    .exec(function (err, user) {
+                        var lb = "Leaderboard for currency \n";
+                        console.log("it is entering the before the for loop");
+                        if(args[1] < 0 || args[1] > 3 || args[1] == null) {
+                            args[1] = 3;
+                        }
+                        for (var i = 0; i < args[1]; i++) {
+                            console.log(user[i].userID);
+                            lb += "\n" + client.users.cache.get(user[i].userID).tag + "\n " + user[i].currency + "<:cash_24:751784973488357457>";
+                            console.log("user[i}.currency" + user[i].currency);
+                            console.log("it is entering the for loop");
+                        }
+                        message.channel.send("``` " + "\n" + lb + "```");
+                    });
+                break;
+            default:
+                message.channel.send("Please enter a valid category to sort by");
+        }
     },
 };
