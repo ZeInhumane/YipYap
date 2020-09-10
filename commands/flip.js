2982c439a768a26c64ae4605f6b0cf4355098807
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2020-09-10 21:46:04 +0800

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -11,17 +11,17 @@ module.exports = {
             }
             else {
                 var bet = parseInt(args[0]);
-                if(args[1] == undefined){
-                    message.channel.send("Invaild syntax");
+                if (args[1] == undefined) {
+                    message.channel.send("Invalid syntax");
                     return;
                 }
                 var faceBetted = args[1].toLowerCase();
-                if(faceBetted != "head" && faceBetted != "tail" && faceBetted != "heads" && faceBetted != "tails"){
-                    message.channel.send("Invaild face of coin betted on");
+                if (faceBetted != "head" && faceBetted != "tail" && faceBetted != "heads" && faceBetted != "tails") {
+                    message.channel.send("Invalid face of coin betted on");
                     return;
                 }
-                if(bet == undefined || isNaN(bet)){
-                    message.channel.send("Invaild amount");
+                if (bet == undefined || isNaN(bet)) {
+                    message.channel.send("Invalid amount");
                 }
                 else if (bet > user.currency) {
                     message.channel.send("You do not have enough money for that bet");
@@ -35,22 +35,22 @@ module.exports = {
                         winLoseStatement = "got";
                     }
                     else if (rng < 5000) {
-                        if(faceBetted == "head" || faceBetted == "heads"){
+                        if (faceBetted == "head" || faceBetted == "heads") {
                             user.currency += bet;
                             winLoseStatement = "got";
                         }
-                        else{
+                        else {
                             user.currency -= bet;
                             winLoseStatement = "lost";
                         }
                         faceLanded = "on heads <:CoinHead:753518321214816297>.";
                     }
                     else {
-                        if(faceBetted == "tail" || faceBetted == "tails"){
+                        if (faceBetted == "tail" || faceBetted == "tails") {
                             user.currency += bet;
                             winLoseStatement = "got";
                         }
-                        else{
+                        else {
                             user.currency -= bet;
                             winLoseStatement = "lost";
                         }
