df45e29bd707a2f7244791ec6fe7095e4a4c1395
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2020-09-10 21:49:47 +0800

    dynamic leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,34 +4,38 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: "leaderboard",
-    description: "Checks leaderboard",
+    description: "Checks leaderboard\n =top {checks by requirement} {leaderboard size}",
     cooldown: 10,
     aliases: ['top'],
     execute(message, args) {
+        var sortBy;
         const client = require('../index.js').client;
         // Find First 10 News Items
         switch (args[0]) {
             case "currency":
-                console.log("it is entering the case");
-                User.find({})
-                    .sort({ currency: -1 })
-                    .exec(function (err, user) {
-                        var lb = "Leaderboard for currency \n";
-                        console.log("it is entering the before the for loop");
-                        if(args[1] < 0 || args[1] > 9 || args[1] == null) {
-                            args[1] = 9;
-                        }
-                        for (var i = 0; i < args[1]; i++) {
-                            console.log(user[i].userID);
-                            lb += "\n" + client.users.cache.get(user[i].userID).tag + "\n " + user[i].currency + " <:cash_24:751784973488357457>";
-                            console.log("user[i}.currency" + user[i].currency);
-                            console.log("it is entering the for loop");
-                        }
-                        message.channel.send("``` " + "\n" + lb + "```");
-                    });
+                sortBy = "currency";
+                break;
+            case "level":
+                sortBy = "level";
                 break;
             default:
                 message.channel.send("Please enter a valid category to sort by");
+                return;
         }
+        User.find({})
+            .sort("-" + sortBy)
+            .exec(function (err, user) {
+                var lb = "Leaderboard for " + sortBy + "\n";
+                console.log("it is entering the before the for loop");
+                if (args[1] < 0 || args[1] > 3 || args[1] == null) {
+                    args[1] = 3;
+                }
+                console.log(' this is sorted by' + sortBy);
+                for (var i = 0; i < args[1]; i++) {
+                    console.log(user[i].userID);
+                    lb += "\n" + client.users.cache.get(user[i].userID).tag + "\n " + user[i][sortBy] + ":person_fencing: ";
+                }
+                message.channel.send("``` " + "\n" + lb + "```");
+            });
     },
 };
