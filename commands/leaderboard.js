9650007654219cbd8773f7945d1160a6b86fb26a
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2021-03-06 17:14:56 +0800

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,45 +1,36 @@
-const User = require('../models/user');
-const mongoose = require('mongoose');
-const Discord = require('discord.js');
-
 module.exports = {
-    name: "leaderboard",
-    description: "Checks leaderboard",
-    syntax: "{Checks by requirement} {Leaderboard size}",
-    cooldown: 10,
-    aliases: ['top'],
-    category: "Fun",
-    execute(message, args) {
-        var sortBy;
-        const client = require('../index.js').client;
-        // Find First 10 News Items
-        switch (args[0]) {
-            case "currency":
-                sortBy = "currency";
-                break;
-            case "level":
-                sortBy = "level";
-                break;
-            default:
-                message.channel.send("Please enter a valid category to sort by");
-                return;
+    execute(message, winner, loser) {
+        const User = require('../../models/user');
+        message.channel.send(winner.player.name + ' defeated ' + (loser.name || loser.player.name) + '!');
+        var moneyEarned = parseInt(loser.level);
+        var commonTreasureChest = false;
+        const itemName = "common";
+        const itemQuantity = 1;
+        // change this to change the chances of getting a lootbox currently set at 50%
+        if(Math.floor(Math.random() * 2 + 1) == 1){
+            commonTreasureChest = true;
         }
-        User.find({})
-            .sort("-" + sortBy)
-            .exec(function (err, user) {
-                var lb = "Leaderboard for " + sortBy + "\n";
-                console.log("it is entering the before the for loop");
-                if (args[1] < 0 || args[1] > 3 || args[1] == null) {
-                    args[1] = 3;
+        User.findOne({ userID: winner.userID }, (err, user) => {
+            if (commonTreasureChest == true){
+                var indexOfItemInInv = user.inv.findIndex((item) => {
+                    if (item.itemName == itemName) {
+                        return true;
+                    }
+                });
+                if (indexOfItemInInv > -1) {
+                    user.inv[indexOfItemInInv].itemQuantity += itemQuantity;
                 }
-                console.log(' this is sorted by' + sortBy);
-                for (var i = 0; i < args[1]; i++) {
-                    console.log(user[i].userID);
-                    lb += "\n" + client.users.cache.get(user[i].userID).tag + "\n " + user[i][sortBy];
-                    console.log(typeof user[i].userID);
+                else {
+                    user.inv.push({ itemName: itemName, itemQuantity: itemQuantity });
                 }
-                message.channel.send("``` " + "\n" + lb + "```");
-            });
-    },
-};
-    
+                user.markModified('inv');
+                message.channel.send(`You've gotten: 1 <a:treasure_chest:815220359699365908> .`);
+            }
+            user.currency += moneyEarned;
+            user.save()
+                    .then(result => console.log(result))
+                    .catch(err => console.error(err));
+        });
+        message.channel.send(winner.player.name + " got " + moneyEarned + "<:cash_24:751784973488357457>");
+    }
+}
