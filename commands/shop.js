45a542866bad5afb5ca98aa7e981a4e0ac7b9078
QuickLittleWolf <70940569+QuickLittleWolf@users.noreply.github.com>
2021-09-06 22:01:31 +0800

    Revert "Update shop.js"
    This reverts commit 244d72e0d37d28395ae48813c19b22dcc4c97e0e.

--- commands/shop.js
+++ commands/shop.js
@@ -5,46 +5,55 @@ module.exports = {
     description: "It is ze shop",
     syntax: "",
     category: "Fun",
-    async execute(message, args) {
+    execute(message, args) {
         const Discord = require('discord.js');
         const Shop = require('../models/shopData');
         const User = require('../models/user');
-        const findItem = require('../functions/findItem.js');
         let onPage = 0;
         let currentColor = '#0099ff';
         // Sets how many items are displayed on a single shop page
         let maxOnPage = 3;
         // Shows max number of page, example if the there are 20 items, there would be 7 pages(Determined by maxOnPage and number of items)
-        let totalItems = await Shop.countDocuments({}).exec();
-        // Lets shop know what the max page to be displayed is
-        let maxPage = Math.floor(totalItems / maxOnPage);
+        let maxPage = 0;
+        let totalItems = 0;
+        Shop.countDocuments({}, function (err, count) {
+            if (err) {
+                (err);
+            }
+            else {
+                totalItems = count;
+            }
+        });
         // Edited shop function
         async function page(user) {
-            function playerTurn(action) {
-                switch (action) {
-                    case "▶️":
-                        onPage++;
-                        break;
-                    case "◀️":
-                        onPage--;
-                        break;
-                    default:
-                        break;
+            function currentPage(pager) {
+                if (pager == 'next') {
+                    onPage++;
+                }
+                else if (pager == 'previous') {
+                    onPage--;
+                }
+                else {
+                    messageDisplayed = "Exited shop";
                 }
             }
 
+            function playerTurn(action) {
+                switch (action) {
+                    case "▶️":
+                        currentPage('next');
+                        break;
+                    case "◀️":
+                        currentPage('previous');
+                        break;
+                    default:
+                        messageDisplayed = "Exited shop";
+                }
+            }
             async function createUpdatedMessage() {
                 let itemNamea;
                 let i = 0;
                 let counter = 0;
-                if (onPage < 0) {
-                    onPage = maxPage;
-                }
-                if (onPage > maxPage) {
-                    onPage = 0;
-                }
-                //i = item i am on
-                i = onPage * maxOnPage;
                 //removing execute apparently fixes await function
                 items = await Shop.find({}).sort("-" + itemNamea)
                 let updatedShopEmbed = new Discord.MessageEmbed()
@@ -53,40 +62,62 @@ module.exports = {
                     .setURL('https://discord.gg/CTMTtQV')
                     .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
                     .setDescription('This is ze shop')
-                    .setFooter(`Current page is ${onPage + 1}/${maxPage + 1}`)
-
-
+                maxPage = Math.ceil(totalItems / maxOnPage);
+                if (onPage < 0) {
+                    onPage = maxPage;
+                }
+                if (onPage >= maxPage) {
+                    onPage = 0;
+                }
+                //i = item i am on
+                i = onPage * maxOnPage;
                 while (i < totalItems && counter < maxOnPage) {
-                    itemInfo = await findItem(items[i].itemName);
-                    updatedShopEmbed.addField(`Item name: ${itemInfo.emote}${items[i].itemName}`, "Item cost: " + items[i].itemCost + "<:cash_24:751784973488357457>");
+                    updatedShopEmbed.addField(`Item name: ${items[i].emote}${items[i].itemName}`, "Item cost: " + items[i].itemCost + "<:cash_24:751784973488357457>");
                     i++;
                     counter++;
                 }
                 return updatedShopEmbed;
             }
 
-            let loop = true;
-            while (loop) {
+            while (playerAction != "❎") {
+                let messageDisplayed, collectorExpireTime;
                 // awaits Player reaction
                 await new Promise((resolve, reject) => {
-                    const collector = botEmbedMessage.createReactionCollector(filter, { time: 60000 });
+                    let timea;
+                    const collector = botEmbedMessage.createReactionCollector(filter, { max: 1, time: 60000 });
                     collector.on('collect', r => {
-                        collector.resetTimer();
-                        console.log(r.emoji.name);
+                        collector.time = 60000;
+                        timea = collector.time;
+                        // Cheap fix to display battle run out time(may change)
+                        clearInterval(collectorExpireTime);
+                        collectorExpireTime = setInterval(function () {
+                            timea -= 1000;
+                        }, 1000);
+                        (r.emoji.name);
                         playerAction = r.emoji.name;
                         resolve();
                     });
-
-                    collector.on('end', async () => {
-                        currentColor = '#FF0000';
-                        botEmbedMessage.edit(await createUpdatedMessage());
-                        loop = false;
+                    // Continued cheap fix
+                    collector.on('end', () => {
+                        
+                        botEmbedMessage.edit(createUpdatedMessage());
+                        if (timea <= 1000) {
+                            currentColor = '#FF0000';
+                            botEmbedMessage.edit(createUpdatedMessage());
+                            message.channel.send('Upgrade expired. Your fatass took too long');
+                            clearInterval(collectorExpireTime);
+                        }
                     });
                 });
-                console.log(playerAction);
+                (playerAction);
                 playerTurn(playerAction);
                 botEmbedMessage.edit(await createUpdatedMessage());
+                ("UPDATE MESSAGE")
             }
+            messageDisplayed = "Stopped upgrading";
+            currentColor = '#FF0000';
+            botEmbedMessage.edit(createUpdatedMessage());
+            clearInterval(collectorExpireTime);
         }
 
         let playerAction = "nothing";
@@ -98,36 +129,34 @@ module.exports = {
             else {
                 filter = (reaction, user) => {
                     ("Check " + reaction.emoji.name);
-                    if ((reaction.emoji.name === '▶️' || reaction.emoji.name === '◀️') && user == message.author.id) {
-                        console.log(reaction.emoji.name + " passed");
+                    if ((reaction.emoji.name === '▶️' || reaction.emoji.name === '◀️' || reaction.emoji.name === '❎') && user == message.author.id) {
+                        (reaction.emoji.name + " passed");
                         return reaction;
                     }
                 };
+                const shopEmbed = new Discord.MessageEmbed()
+                    .setColor(currentColor)
+                    .setTitle(user.player.name + '\'s currency: ' + user.currency)
+                    .setURL('https://discord.gg/CTMTtQV')
+                    .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
+                    .setDescription('This is ze shop')
                 let itemName;
                 let i = 0;
                 let counter = 0;
                 Shop.find({})
                     .sort("-" + itemName)
-                    .exec(async function (err, items) {
-
+                    .exec(function (err, item) {
+                        ("it is entering the before the for loop 2");
+                        maxPage = Math.ceil(totalItems / maxOnPage);
                         if (onPage < 0) {
                             onPage = maxPage;
                         }
-                        if (onPage > maxPage) {
+                        if (onPage >= maxPage) {
                             onPage = 0;
                         }
                         i = onPage * maxOnPage;
-                        const shopEmbed = new Discord.MessageEmbed()
-                            .setColor(currentColor)
-                            .setTitle(user.player.name + '\'s currency: ' + user.currency)
-                            .setURL('https://discord.gg/CTMTtQV')
-                            .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
-                            .setDescription('This is ze shop')
-                            .setFooter(`Current page is ${onPage + 1}/${maxPage + 1}`)
-
                         while (i < totalItems && counter < maxOnPage) {
-                            itemInfo = await findItem(items[i].itemName);
-                            shopEmbed.addField(`Item name: ${itemInfo.emote}${items[i].itemName}`, "Item cost: " + items[i].itemCost + "<:cash_24:751784973488357457>");
+                            shopEmbed.addField(`Item name: ${item[i].emote}${item[i].itemName}`, "Item cost: " + item[i].itemCost + "<:cash_24:751784973488357457>");
                             i++;
                             counter++;
                         }
@@ -136,10 +165,13 @@ module.exports = {
                                 botEmbedMessage = botMessage;
                                 botMessage.react("◀️");
                                 botMessage.react("▶️");
+                                botMessage.react("❎");
                                 page(user);
                             });
                     });
+
             }
         });
+
     }
 }
