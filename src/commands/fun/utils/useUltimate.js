a228abffe27d3d778e2506afa7124cfb3de97813
0xfinder <49021865+0xfinder@users.noreply.github.com>
2022-02-18 12:43:10 +0800

    Update useUltimate.js

--- src/commands/fun/utils/useUltimate.js
+++ src/commands/fun/utils/useUltimate.js
@@ -1,11 +1,21 @@
 const ultimateBase = require('../interface/ultimateBase');
 
 module.exports = async function (player, enemy, user) {
+
+    console.log(user.rune);
     // If out of range or user has no rune, default to rune 3 (lifesteal)
     if (!user.rune || user.rune > ultimateBase.ultimates.length) { user.rune = 3; }
 
+    console.log(user.rune);
+
+    // Create new ultimate class
     const ultimate = new ultimateBase.ultimates[user.rune](player, enemy, user);
+    console.log(ultimate.name);
+
+    // Get results
     const results = ultimate.ultimate(player, enemy, user);
+
+    // Display message
     const message = ultimate.displayMessage(player, results[0], results[1]);
 
     return message;
