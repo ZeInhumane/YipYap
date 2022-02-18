555f719fde7f16341265316e1a37254ffab33dce
0xfinder <49021865+0xfinder@users.noreply.github.com>
2022-02-18 19:59:33 +0800

    Edit battle, remove areas

--- src/commands/area/areas/Ludus.js
+++
@@ -1,9 +0,0 @@
-const AreaInterface = require('../AreaInterface.js');
-
-module.exports = class Ludus extends AreaInterface {
-    init() {
-        this.name = "Ludus";
-        this.id = 1;
-        this.desc = "Ludus is a planet in OASIS created by Gregarious Simulation Systems to serve as the primary establishment for public education within the simulation.";
-    }
-};
--- src/commands/fun/battle.js
+++ src/commands/fun/battle.js
@@ -1,10 +1,8 @@
-const Discord = require('discord.js');
 const botLevel = require('../../models/botLevel');
-const User = require('../../models/user');
 const win = require('../../classes/battle/win.js');
-const userEffects = require('../../models/userEffects.js');
-const findPrefix = require('../../functions/findPrefix');
-const useUltimate = require('./utils/useUltimate.js');
+const ticketUtil = require('./utils/ticketUtil.js');
+const battleUtil = require('./utils/battleUtil.js');
+const Battle = require('./interface/battleInterface.js');
 
 module.exports = {
     name: "battle",
@@ -13,345 +11,29 @@ module.exports = {
     cooldown: 20,
     aliases: ['b'],
     category: "Fun",
-    // change timing in main bot
-    async execute({ message }) {
-        const ultimateEmote = ":Ultimate:822042890955128872";
-        const emptyUltimateEmote = "<:blank:829270386986319882>";
-        const ultimateEmoteArray = ["<:1:829267948127649792>", "<:2:829267958836101130>", "<:3_:829267967392088134>", "<:4:829267977559867412>", "<:5:829271937548419093>",
-            "<:6:829271966161567774>", "<:7:829271980397166612>", "<:8:829271994205208597>", "<:9:829272014946697246>", "<:10:829272027604713523>"];
-        const row = new Discord.MessageActionRow()
-            .addComponents(
-                new Discord.MessageButton()
-                    .setCustomId('attack')
-                    .setLabel('⚔️')
-                    .setStyle('PRIMARY'),
-                new Discord.MessageButton()
-                    .setCustomId('defend')
-                    .setLabel('🛡️')
-                    .setStyle('PRIMARY'),
-                new Discord.MessageButton()
-                    .setCustomId('ultimate')
-                    .setLabel('')
-                    .setStyle('DANGER')
-                    .setEmoji(ultimateEmote),
-            );
-        let displayUltimateString = `<:Yeet:829267937784627200>${emptyUltimateEmote.repeat(10)}<:Yeet2:829270362516488212>`;
+    async execute({ message, user }) {
+        // Get effects messages to display in embed later
+        const { expMsg, goldMsg } = await ticketUtil.ticketEffects(message.author.id, user, message);
 
-        let currentColor = '#0099ff';
-        let ultimate = 0;
+        // Get locationInfo
         let locationInfo;
-        let originalPlayerHP, originalEnemyHP;
+        await botLevel.findOne({ 'Location': user.location }, (err, result) => { locationInfo = result._doc; });
 
-        // Method to check for damage taken by hero
-        function takeDamage(damage, defender, isHero) {
-            let attMulti = damage / defender.defense;
-            if (attMulti < 0.4) {
-                attMulti = 0.4;
-            } else if (attMulti > 1.5) {
-                attMulti = 1.5;
-            }
+        // Create enemy
+        const enemy = battleUtil.makeNewEnemy(user, locationInfo);
 
-            let damageTaken = Math.floor((damage + Math.floor((damage - defender.defense) / 4)) * attMulti);
-            // Ensures damage taken is at least 1
-            if (damageTaken < 1) {
-                damageTaken = 1;
-                if (isHero) {
-                    ultimate += 18;
-                }
-            }
+        // Initialize battle
+        const battle = new Battle(user, enemy, locationInfo);
 
-            if (isHero) {
-                if (playerAction == "defend") {
-                    // Change it later so higher level reduces damagetaken too
-                    if (defender.defense > 99) {
-                        damageTaken *= 1 / 100;
-                        ultimate += 24;
-                    } else {
-                        damageTaken *= (100 - defender.defense) / 100;
-                        ultimate += 20;
-                    }
-                } else {
-                    ultimate += 20;
-                }
-                // Ensures ultimate charge does not pass 100(max)
-                if (ultimate > 100) {
-                    ultimate = 100;
-                }
-                displayUltimateString = `<:Yeet:829267937784627200>${ultimateEmoteArray.slice(0, Math.floor((ultimate) / 10)).join("")}${emptyUltimateEmote.repeat(Math.ceil((100 - ultimate) / 10))}<:Yeet2:829270362516488212>`;
-            }
-            damageTaken = Math.floor(damageTaken);
-            defender.hp -= damageTaken;
-            return damageTaken;
+        // Returns a boolean of whether player won
+        const playerWon = await battle.initBattle(message, expMsg, goldMsg);
+
+        if (playerWon) {
+            win.execute(message, user, enemy, locationInfo);
+        } else if (playerWon == false) {
+            message.channel.send(`${user.player.name} has been defeated by ${enemy.name}!`);
+        } else {
+            return;
         }
-
-        // Creates Enemy class
-        class Enemy {
-            constructor(name, hp, attack, defense, speed, type, lvl) {
-                this.name = name;
-                this.hp = hp;
-                this.attack = attack;
-                this.defense = defense;
-                this.speed = speed;
-                this.type = type;
-                this.level = lvl;
-            }
-        }
-
-        // Battle function
-        async function battle(user, player, enemy, expMsg, goldMsg, botEmbedMessage) {
-            let playerTurnAction = "nothing";
-            let enemyTurnAction = "nothing";
-
-            function dodgeAttack(attacker, defender) {
-                if (defender.speed > attacker.speed) {
-                    const speedDifference = defender.speed - attacker.speed;
-                    let rng = speedDifference / attacker.speed;
-                    if (rng > 0.2) {
-                        rng = 0.2;
-                    }
-                    const dodge = Math.random() <= rng;
-                    return dodge;
-                }
-                return false;
-            }
-
-            async function playerTurn(action) {
-                if (action == "attack") {
-
-
-                    if (dodgeAttack(player, enemy)) {
-                        playerTurnAction = `${player.name}'s turn!\n${player.name} attacked but ${enemy.name} dodged!\n`;
-                    } else {
-                        playerTurnAction = `${player.name}'s turn!\n${player.name} does ${takeDamage(player.attack, enemy, false)} damage!\n`;
-                    }
-                } else if (action == "defend") {
-                    playerTurnAction = "You shield yourself, it works";
-                } else if (action == "ultimate") {
-                    if (ultimate == 100) {
-                        // set ultimate charge
-                        ultimate = 0;
-
-                        // Change ult button to red
-                        row.components[2].setStyle('DANGER');
-
-                        playerTurnAction = await useUltimate(player, enemy, user);
-                        displayUltimateString = `<:Yeet:829267937784627200>${emptyUltimateEmote.repeat(10)}<:Yeet2:829270362516488212>`;
-                    } else {
-                        playerTurnAction = `You only have ${ultimate} ultimate charge, you need 100 to use your ultimate.`;
-                    }
-                } else {
-                    playerTurnAction = "Nothing happened";
-                }
-
-            }
-            // Gives an Enemy (Probably add shielding here)
-            function enemyTurn() {
-                if (dodgeAttack(enemy, player)) {
-                    enemyTurnAction = `${enemy.name}'s turn!\n${enemy.name} attacked but ${player.name} dodged!\n`;
-                } else {
-                    enemyTurnAction = `${enemy.name}'s turn!\n${enemy.name} does ${takeDamage(enemy.attack, player, true)} damage!\n`;
-                }
-            }
-
-            // Updates battle embed to display ongoing input
-            async function createUpdatedMessage() {
-                const updatedBattleEmbed = new Discord.MessageEmbed()
-                    .setColor(currentColor)
-                    .setTitle(user.player.name + '\'s ultimate charge: ' + ultimate + "/100")
-                    .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
-                    .setDescription(displayUltimateString)
-                    .addFields(
-                        { name: 'Experience Ticket', value: expMsg, inline: true },
-                        { name: 'Gold Ticket', value: goldMsg, inline: true },
-                        { name: 'Player HP', value: `Lvl ${user.level} **${player.name}**'s **HP**: ${player.hp}/${originalPlayerHP}` },
-                        { name: 'Enemy HP', value: `Lvl ${enemy.level} **${enemy.name}**'s **HP**: ${enemy.hp}/${originalEnemyHP}` },
-                        { name: 'Turn', value: playerTurnAction },
-                        { name: '​', value: enemyTurnAction },
-                    )
-                    .setImage(locationInfo.LocationImage)
-                    .setFooter(`${locationInfo.Description}`);
-                return updatedBattleEmbed;
-            }
-
-            // Battle goes on when Player and Enemy is still alive
-            let isExpired = false;
-            // Filter so only user can interact with the buttons
-            const filter = i => {
-                i.deferUpdate();
-                return i.user.id === message.author.id;
-            };
-            while (player.hp > 0 && enemy.hp > 0 && !isExpired) {
-                // awaits Player reaction
-                await botEmbedMessage.awaitMessageComponent({ filter, componentType: 'BUTTON', time: 60000 })
-                    .then(async i => {
-                        currentColor = '#0099ff';
-                        playerAction = i.customId;
-
-                        // Checks for who has first turn
-                        if (player.speed > enemy.speed) {
-                            playerTurn(playerAction);
-                            if (enemy.hp > 0) {
-                                enemyTurn();
-                            } else {
-                                enemyTurnAction = 'Enemy has been defeated!';
-                            }
-                        } else {
-                            enemyTurn();
-                            if (player.hp > 0) playerTurn(playerAction);
-                        }
-                        if (enemy.hp < 0) {
-                            enemy.hp = 0;
-                            currentColor = '#FF0000';
-                        }
-                        if (player.hp < 0) {
-                            player.hp = 0;
-                            currentColor = '#FF0000';
-                        }
-                        if (ultimate == 100) {
-                            // Change ult button to green
-                            row.components[2].setStyle('SUCCESS');
-                        }
-                        botEmbedMessage.edit({ embeds: [await createUpdatedMessage(expMsg, goldMsg)], components: [row] });
-                    })
-                    .catch(async () => {
-                        currentColor = '#FF0000';
-                        botEmbedMessage.edit({ embeds: [await createUpdatedMessage(expMsg, goldMsg)], components: [] });
-                        message.channel.send('Battle expired. Your fatass took too long');
-                        isExpired = true;
-                    });
-            }
-            // Removes buttons
-            botEmbedMessage.edit({ embeds: [await createUpdatedMessage(expMsg, goldMsg)], components: [] });
-
-            if (!isExpired) {
-                // Checks for who won
-                if (player.hp > 0) {
-                    win.execute(message, user, enemy, locationInfo);
-                } else {
-                    message.channel.send(`${player.name} has been defeated by ${enemy.name}!`);
-                }
-            }
-        }
-
-        // Makes new random enemy
-        async function makeNewEnemy(user) {
-            let enemyLvl = Math.floor(Math.random() * 11) - 5 + user.level;
-            if (enemyLvl < 1) enemyLvl = 1;
-            const baseStat = enemyLvl / 2 < 1 ? Math.floor(enemyLvl / 2) : 1;
-            const minStat = 5;
-            // Takes the buff from the db and applies it to the enemies
-            const { hp: hpMulti, attack: attackMulti, defense: defenseMulti, speed: speedMulti } = locationInfo.Buff;
-            const enemyHP = Math.floor((Math.random() * (Math.exp(enemyLvl) ** (1 / 20)) + minStat * 5 + enemyLvl * 5) * hpMulti);
-            const enemyAttack = Math.floor((Math.random() * enemyLvl + minStat + baseStat) * attackMulti);
-            const enemyDefense = Math.floor((Math.random() * enemyLvl + minStat + baseStat) * defenseMulti);
-            const enemySpeed = Math.floor((Math.random() * enemyLvl + minStat + baseStat) * speedMulti);
-            const enemyType = "undead";
-            const enemy = new Enemy(locationInfo.Enemy, enemyHP, enemyAttack, enemyDefense, enemySpeed, enemyType, enemyLvl);
-            return enemy;
-        }
-
-        let playerAction;
-        User.findOne({ userID: message.author.id }, async (err, user) => {
-            if (user == null) {
-                // Getting the prefix from db
-                const prefix = await findPrefix(message.guild.id);
-                message.channel.send(`You have not set up a player yet! Do ${prefix}start to start.`);
-            } else {
-                let expMsg = 'No active EXP Ticket.';
-                let goldMsg = 'No active Gold Ticket.';
-                await userEffects.findOne({ userID: message.author.id }, async (err, effects) => {
-
-                    if (effects != null) {
-                        const tickets = Object.keys(effects.tickets);
-                        for (let i = 0; i < tickets.length; i++) {
-                            const ticketName = tickets[i];
-                            const ticket = effects.tickets[ticketName];
-                            const today = new Date();
-                            if (today >= ticket.endTime) {
-                                let msg = `You do not have auto active. Your ${ticketName} has been used up.`;
-                                if (ticket.auto == 'true') {
-                                    await User.findOne({ userID: message.author.id }, async () => {
-                                        if (user.inv[ticketName]) {
-                                            msg = `Auto is active, another ${ticketName} has been activated.`;
-                                            user.inv[ticketName].quantity -= 1;
-                                            ticket.startTime = new Date();
-                                            Date.prototype.addHours = function (h) {
-                                                this.setHours(this.getHours() + h);
-                                                return this;
-                                            };
-                                            ticket.endTime = new Date().addHours(ticket.duration);
-                                            if (user.inv[ticketName].quantity == 0) {
-                                                msg += ` This is your last ${ticketName}.`;
-                                                delete user.inv[ticketName];
-                                            }
-
-                                        } else {
-                                            msg = `Your inventory does not have any more ${ticketName}. auto will be deactivated.`;
-                                            delete effects.tickets[ticketName];
-                                        }
-                                        user.markModified('inv');
-                                        user.save()
-                                            .then(result => console.log(result))
-                                            .catch(err => console.error(err));
-                                    });
-                                } else {
-                                    delete effects.tickets[ticketName];
-                                }
-                                message.channel.send(msg);
-                            }
-                        }
-                        const expTicketName = Object.keys(effects.tickets).filter(key => key.includes('Experience'))[0];
-                        const expTicketObject = effects.tickets[expTicketName];
-                        const goldTicketName = Object.keys(effects.tickets).filter(key => key.includes('Gold'))[0];
-                        const goldTicketObject = effects.tickets[goldTicketName];
-                        if (expTicketName) {
-                            expMsg = `${expTicketName} active: Boost Experience gained by ${expTicketObject.multiplier}`;
-                        }
-                        if (goldTicketName) {
-                            goldMsg = `${goldTicketName} active: Boost Gold gained by ${goldTicketObject.multiplier}`;
-                        }
-                        effects.markModified('tickets');
-                        effects.save()
-                            .then(result => console.log(result))
-                            .catch(err => console.error(err));
-                    }
-                });
-                await botLevel.findOne({ 'Location': user.location }, (err, result) => { locationInfo = result._doc; });
-                const enemy = await makeNewEnemy(user);
-
-
-                const player = { name: user.player.name };
-
-                for (const stat in user.player.baseStats) {
-                    player[stat] = Math.round(user.player.baseStats[stat] * (1 + user.player.additionalStats[stat].multi / 100) + user.player.additionalStats[stat].flat);
-                }
-
-                originalPlayerHP = player.hp;
-                originalEnemyHP = enemy.hp;
-                // Makes battle embed
-                const battleEmbed = new Discord.MessageEmbed()
-                    .setColor(currentColor)
-                    .setTitle(user.player.name + '\'s ultimate charge: ' + ultimate + "/100")
-                    .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
-                    .setDescription(displayUltimateString)
-                    .addFields(
-                        { name: 'Experience Ticket', value: expMsg, inline: true },
-                        { name: 'Gold Ticket', value: goldMsg, inline: true },
-                        { name: 'Player HP', value: `Lvl ${user.level} **${player.name}**'s **HP**: ${player.hp}/${originalPlayerHP}` },
-                        { name: 'Enemy HP', value: `Lvl ${enemy.level} **${enemy.name}**'s **HP**: ${enemy.hp}/${originalEnemyHP}` },
-                    )
-                    .setImage(locationInfo.LocationImage)
-                    .setFooter(`${locationInfo.Description}`);
-
-                message.channel.send({ embeds: [battleEmbed], components: [row] })
-                    .then(botMessage => {
-                        battle(user, player, enemy, expMsg, goldMsg, botMessage);
-                    });
-            }
-        });
-
-
     },
-
-
 };
