aabbc351a672a51627c777e1395f1b3de4021dd1
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2022-02-19 15:44:03 +0800

    new upgrade + a bit of uneccessary complexity

--- src/commands/fun/newUpgrade.js
+++ src/commands/fun/newUpgrade.js
@@ -3,16 +3,45 @@ const User = require('../../models/user');
 const findPrefix = require('../../functions/findPrefix');
 
 module.exports = {
-    name: "reset",
-    description: "Resets your stats. Special Points will be credited.",
+    name: "newupgrade",
+    description: "Upgrade your stats using special points. Special Points are earned through leveling.",
     syntax: "",
-    aliases: ['r', 'resetsp'],
+    aliases: ['nu'],
     category: "Fun",
     execute({ message }) {
         let currentColor = "#0099ff";
+        let multi = 1;
         // Buttons
         const row1 = new Discord.MessageActionRow()
             .addComponents(
+                new Discord.MessageButton()
+                    .setCustomId('hp')
+                    .setLabel('❤️')
+                    .setStyle('PRIMARY'),
+                new Discord.MessageButton()
+                    .setCustomId('attack')
+                    .setLabel('⚔️')
+                    .setStyle('PRIMARY'),
+                new Discord.MessageButton()
+                    .setCustomId('defense')
+                    .setLabel('🛡️')
+                    .setStyle('PRIMARY'),
+                new Discord.MessageButton()
+                    .setCustomId('speed')
+                    .setLabel('💨')
+                    .setStyle('PRIMARY'),
+            );
+
+        const row2 = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setCustomId('up')
+                    .setLabel('⬆️')
+                    .setStyle('PRIMARY'),
+                new Discord.MessageButton()
+                    .setCustomId('down')
+                    .setLabel('⬇️')
+                    .setStyle('PRIMARY'),
                 new Discord.MessageButton()
                     .setCustomId('confirm')
                     .setLabel('Confirm')
@@ -26,11 +55,10 @@ module.exports = {
             );
 
         // Filter so only user can interact with the buttons
-        const filter = (btnInt) => {
-            btnInt.deferUpdate();
-            return btnInt.user.id === message.author.id;
+        const filter = i => {
+            i.deferUpdate();
+            return i.user.id === message.author.id;
         };
-
         let userSelection, messageDisplayed;
         // Reset prompt
         async function reset(user, botEmbedMessage, sp) {
@@ -42,7 +70,7 @@ module.exports = {
                         userSelection = btnInt.customId;
                         // Parse button pressed
                         parse(userSelection);
-                        botEmbedMessage.edit({ embeds: [await createUpdatedMessage(botEmbedMessage)], components: [] });
+                        botEmbedMessage.edit({ embeds: [await createUpdatedMessage(user)], components: [row1, row2] });
                     })
                     .catch(async (error) => {
                         currentColor = '#FF0000';
@@ -58,6 +86,24 @@ module.exports = {
             // Parse button pressed
             async function parse(action) {
                 switch (action) {
+                    case "up":
+                        multiplier('up');
+                        break;
+                    case "down":
+                        multiplier('down');
+                        break;
+                    case "hp":
+                        upgrade('hp');
+                        break;
+                    case "attack":
+                        upgrade('attack');
+                        break;
+                    case "defense":
+                        upgrade('defense');
+                        break;
+                    case "speed":
+                        upgrade('speed');
+                        break;
                     case "confirm":
                         // Deactivates buttons and displays confirmation message
                         messageDisplayed = 'Awaiting confirmation...';
@@ -65,33 +111,83 @@ module.exports = {
                         break;
                     case "cancel":
                         // Cancel message
-                        messageDisplayed = "Stats were not reset";
+                        messageDisplayed = "Upgrade did not pass validation. Cancelling...";
                         currentColor = '#FF0000';
                         break;
                 }
             }
-        }
+            async function multiplier(playeraction) {
+                switch (playeraction) {
+                    case 'up':
+                        if (multi < 5) {
+                            multi += 1;
+                            messageDisplayed = 'Multiplier increased by 1';
+                        } else {
+                            messageDisplayed = 'Multiplier cannot exceed 5';
+                        }
+                        break;
+                    case 'down':
+                        if (multi > 1) {
+                            multi -= 1;
+                            messageDisplayed = 'Multiplier decreased by 1';
+                        } else {
+                            messageDisplayed = 'Multiplier cannot go below 1';
+                        }
+                }
+            }
 
-        // Updates embed message
-        async function createUpdatedMessage(embedMessage) {
-            const receivedEmbed = embedMessage.embeds[0];
-            const updatedBattleEmbed = new Discord.MessageEmbed(receivedEmbed)
+            async function upgrade(stat_str) {
+                if (user.sp >= multi) {
+                    user.sp -= multi;
+                    if (stat_str == 'hp') {
+                        user.player.baseStats[stat_str] += 5 * multi;
+                        messageDisplayed = `Your ${stat_str} stat has been upgraded by ${5 * multi}!`;
+                    } else {
+                        console.log("Its entering");
+                        console.log(multi);
+                        user.player.baseStats[stat_str] += multi;
+                        messageDisplayed = `Your ${stat_str} stat has been upgraded by ${multi}!`;
+                    }
+                } else {
+                    messageDisplayed = 'Not enough sp to upgrade.';
+                }
+            }
+
+        }
+        async function createUpdatedMessage(user) {
+            // theres a difference here check later for something
+            const updatedBattleEmbed = new Discord.MessageEmbed()
                 .setColor(currentColor)
+                .setTitle(user.player.name + '\'s sp: ' + user.sp)
+                .setURL('https://discord.gg/CTMTtQV')
+                .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
+                .setDescription('Upgrade your character using sp earned every level up!')
+                .addFields(
+                    { name: `:level_slider: ${user.level}`, value: "\u200b" },
+                    { name: `Current Statistics for player`, value: "\u200b" },
+                    { name: `Current Health (:hearts:): ${user.player.baseStats.hp}`, value: `(+${5 * multi})`, inline: true },
+                    { name: `Current Attack (:crossed_swords:): ${user.player.baseStats.attack}`, value: `(+${1 * multi})`, inline: true },
+                    { name: `Current Defense (:shield:): ${user.player.baseStats.defense}`, value: `(+${1 * multi})`, inline: true },
+                    { name: `Current Speed (:dash:): ${user.player.baseStats.speed}`, value: `(+${1 * multi})`, inline: true },
+                    { name: `sp: ${user.sp}`, value: `${multi} used per upgrade` },
+                )
                 .addField('Update: ', messageDisplayed);
+
+
             return updatedBattleEmbed;
         }
 
         // Confirm reset
-        async function confirm(user, botEmbedMessage, sp) {
+        async function confirm(user, botEmbedMessage) {
             // Creates confirmation message
             const confirmationEmbed = new Discord.MessageEmbed()
                 .setColor(currentColor)
                 .setTitle('Are you sure you would like to proceed?')
                 .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
-                .setDescription('This will reset your stats and give you special points.')
-                .setFooter('Note: This action is irreversible.');
+                .setDescription('This will use up your sp.')
+                .setFooter('Note: This action is irreversible (actually it is but :D).');
 
-            const confirmation = await botEmbedMessage.reply({ embeds: [confirmationEmbed], components: [row1], ephemeral: true });
+            const confirmation = await botEmbedMessage.reply({ embeds: [confirmationEmbed], components: [row1, row2], ephemeral: true });
 
             // Confirmation interaction collector
             await confirmation.awaitMessageComponent({ filter, componentType: 'BUTTON', time: 60000 })
@@ -100,11 +196,6 @@ module.exports = {
                     userSelection = btnInt.customId;
                     // If user selected confirm, reset stats, give sp
                     if (userSelection == 'confirm') {
-                        // Reset stats
-                        user.player.baseStats = { hp: 50, attack: 5, defense: 5, speed: 5 };
-                        // Add sp
-                        user.sp += sp;
-                        // Log
                         user.markModified('player');
                         user.save()
                             .then(() => console.log("reset"))
@@ -112,8 +203,8 @@ module.exports = {
                         // Send success embed
                         const successEmbed = new Discord.MessageEmbed(botEmbedMessage.embeds[0])
                             .setColor('#77DD66')
-                            .setTitle('Stats were reset!')
-                            .setDescription(`Your stats have been reset and you have been credited with ${sp} special points.`)
+                            .setTitle('Successfully upgraded!')
+                            .setDescription(`You have successfully assigned your Special Points`)
                             .setFooter('Spend your special points with the upgrade command!');
                         successEmbed.fields = [];
                         try {
@@ -136,7 +227,7 @@ module.exports = {
                             const retryEmbed = await createUpdatedMessage(botEmbedMessage);
                             retryEmbed.fields.pop();
                             // Re-add buttons to original message
-                            await botEmbedMessage.edit({ embeds: [retryEmbed], components: [row1] });
+                            await botEmbedMessage.edit({ embeds: [retryEmbed], components: [row1, row2] });
                         } catch (error) {
                             // If original message was deleted
                             if (error.code == Discord.Constants.APIErrors.UNKNOWN_MESSAGE) {
@@ -159,7 +250,7 @@ module.exports = {
                             const retryEmbed = await createUpdatedMessage(botEmbedMessage);
                             retryEmbed.fields.pop();
                             // Send message along with buttons
-                            await botEmbedMessage.edit({ embeds: [retryEmbed], components: [row1] });
+                            await botEmbedMessage.edit({ embeds: [retryEmbed], components: [row1, row2] });
                         } catch (err) {
                             // If original message was deleted too, return
                             if (error.code == Discord.Constants.APIErrors.UNKNOWN_MESSAGE) {
@@ -184,38 +275,31 @@ module.exports = {
                 return;
             }
 
-            // For users who were created before sp was created
-            if (user.sp == null) {
-                user.sp = (user.level - 1) * 5;
-            }
-
             // SP to be gained from reset
             const hpSp = (user.player.baseStats.hp - 50) / 5;
-            // SP to be gained from reset (loops through remainder values, yes I am aware this was a complete was of time future matthew)
-            const spGain = Object.values(user.player.baseStats).slice(1).reduce((total, n) => total + n - 5, 0) + hpSp;
+            const attackSp = user.player.baseStats.attack - 5;
+            const defenseSp = user.player.baseStats.defense - 5;
+            const speedSp = user.player.baseStats.speed - 5;
+            const spGain = hpSp + attackSp + defenseSp + speedSp;
 
-            if (spGain == 0) {
-                message.channel.send(`You do not have any sp to reset!`);
-                return;
-            }
 
             // Make reset sp embed
             const spEmbed = new Discord.MessageEmbed()
                 .setColor(currentColor)
-                .setTitle('Reset your stats?')
+                .setTitle('Upgrade your stats!')
                 .setAuthor(message.member.user.tag, message.author.avatarURL(), 'https://discord.gg/h4enMADuCN')
-                .setDescription('Current stats')
+                .setDescription('Upgrade your character using sp earned every level up!')
                 .addFields(
                     { name: `Current Level :level_slider: ${user.level}`, value: "\u200b" },
                     { name: `Current Statistics for player`, value: "\u200b" },
                     { name: `Current Health (:hearts:): ${user.player.baseStats.hp}`, value: `Assigned SP: ${hpSp}`, inline: true },
-                    { name: `Current Attack (:crossed_swords:): ${user.player.baseStats.attack}`, value: `Assigned SP: ${user.player.baseStats.attack - 5}`, inline: true },
-                    { name: `Current Defense (:shield:): ${user.player.baseStats.defense}`, value: `Assigned SP: ${user.player.baseStats.defense - 5}`, inline: true },
-                    { name: `Current Speed (:dash:): ${user.player.baseStats.speed}`, value: `Assigned SP: ${user.player.baseStats.speed - 5}`, inline: true },
+                    { name: `Current Attack (:crossed_swords:): ${user.player.baseStats.attack}`, value: `Assigned SP: ${attackSp}`, inline: true },
+                    { name: `Current Defense (:shield:): ${user.player.baseStats.defense}`, value: `Assigned SP: ${defenseSp}`, inline: true },
+                    { name: `Current Speed (:dash:): ${user.player.baseStats.speed}`, value: `Assigned SP: ${speedSp}`, inline: true },
                 )
                 .setFooter(`SP to be gained: ${spGain}`);
 
-            message.channel.send({ embeds: [spEmbed], components: [row1] })
+            message.channel.send({ embeds: [spEmbed], components: [row1, row2] })
                 .then(botMessage => {
                     reset(user, botMessage, spGain);
                 });
