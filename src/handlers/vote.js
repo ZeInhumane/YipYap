f1715363ea002f80166fd7084b441ed2f8ef41d0
0xfinder <49021865+0xfinder@users.noreply.github.com>
2022-02-23 18:04:58 +0800

    Update vote.js

--- src/handlers/vote.js
+++ src/handlers/vote.js
@@ -1,10 +1,9 @@
 const User = require('../models/user');
 const client = require('../../index.js').client;
 module.exports = (userID) => {
-
-    client.emit('vote', userID, user);
     const user = User.findOne({ userID: userID.id });
     if (!user) {
         return;
     }
+    client.emit('vote', userID, user);
 };
