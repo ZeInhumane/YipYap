7742716b625d3a2710b8d4c910e4cd0168b2cc61
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2022-02-23 15:33:01 +0800

    Revert "Update webhook.js"
    This reverts commit 3718076f60d4ab41ec8c225a3f21c2866cf1c57b.

--- src/handlers/webhook.js
+++ src/handlers/webhook.js
@@ -1,29 +1,49 @@
-const express = require('express');
-const Topgg = require('@top-gg/sdk');
-const fetch = require('node-fetch');
+// // Install discord.js module with `npm install discord.js`
+// const Discord = require('discord.js');
+// // token is the discord bot token, apiToken is the top.gg API token
+// const { token } = require('./config.json');
+const dotenv = require('dotenv');
 
-const app = express();
-const webhook = new Topgg.Webhook(process.env.token);
-module.exports = () => {
-    app.post('/vote', webhook.listener(vote => { // ending url
-        console.log("User with id - " + vote.user + " just voted!");
-        let value = JSON.stringify({
-            embeds: [
-                {
-                    title: "Thanks for voting",
-                    description: '<@${vote.user}> Just voted for Bot!',
-                    color: "8388736" //Hex -> Decimal
-                }
-            ]
-        })
-        fetch("WEBHOOK", {
-            methon: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: value
-        }).catch(e => console.log('Error accured while posting webhook : ' + e));
-    }));
-    app.listen(process.env.PORT);
-    console.log("Vote log is ready!");
+// Build the process.env object
+dotenv.config();
+
+
+
+// THIS IS THE IMPORTANT PART
+
+// Get the top.gg API token from the config file
+const { apiToken } = require('./config.json');
+
+// Make sure to install this with 'npm install dblapi.js`
+const DBL = require('dblapi.js');
+// The webhookPort can be whatever you want but make sure you open that port in the firewall settings (for linux for example you can use `sudo ufw allow 8000`)
+// The webhookAuth is set by you, make sure you keep it secure and don\'t leak it
+const dbl = new DBL(process.env.apiToken, { webhookPort: process.env.PORT || 3000, webhookAuth: process.env.webhookAuth });
+module.exports = (client) => {
+    // When the webhook is ready log it to the console, this will log `Webhook up and running at http://0.0.0.0:8000/dblwebhook`
+    dbl.webhook.on('ready', hook => {
+        console.log(`Webhook up and running at http://${hook.hostname}:${hook.port}${hook.path}`);
+    });
+
+    // This will just log errors if there are any
+    dbl.on('error', e => {
+        console.log(`Oops! ${e}`);
+    });
+
+    // When the webhook receives a vote
+    dbl.webhook.on('vote', async vote => {
+        // This will log the whole vote object to the console
+        console.log(vote);
+        // Get the Discord ID of the user who voted
+        const userID = vote.user;
+
+        // // Variable for the channel were we'll send messages when users vote for the bot
+        // let channelForWebhooks;
+        // Get the Discord Channel were we will send the message whenever a user votes for the bot
+        // Replace channelID with a valid Discord Channel ID were your bot can send messages too
+        const channelForWebhooks = await client.channels.resolve('945934883421839360');
+        // To my one and only god, NotErwin do you approve this 0.0? 
+        // If the channel to send messages in exists, we send a message in it with the ID of the user who votes
+        if (channelForWebhooks) await channelForWebhooks.send(`User with ID \`${userID}\` just voted!`);
+    })
 };
