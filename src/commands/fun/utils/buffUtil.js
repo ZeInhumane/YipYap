0ad8b9cc6f817af8184b9b1078f3765f760cb831
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2022-02-27 16:18:16 +0800

    Update buffUtil.js

--- src/commands/fun/utils/buffUtil.js
+++ src/commands/fun/utils/buffUtil.js
@@ -1,12 +1,7 @@
 const buffInterface = require('../interface/buffInterface');
 
 module.exports = async function (player, enemy, buffID) {
-    console.log("it is entering the buff interface");
-    console.log("player" + player);
-    console.log(player.buffs);
-    console.log(player.buffs);
-    console.log("buffID: " + buffID);
-    // Create new ultimate class
+    // Create new buff class
     const buff = new buffInterface.buffs[buffID](player, enemy, buffID);
 
     // Get results
