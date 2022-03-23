2cc31d852dc4596d6592c7d41b9f4040e30c1d2a
finder <0xfinder@protonmail.com>
2022-03-24 00:15:09 +0800

    use pagination for areas

--- src/commands/areas/areas.js
+++ src/commands/areas/areas.js
@@ -1,37 +1,62 @@
 const AreaInterface = require("./AreaInterface.js");
+const { MessageEmbed } = require("discord.js");
+const { PaginateContent } = require("../../functions/pagination/Pagination");
 
 module.exports = {
     name: "areas",
     aliases: [""],
     category: "Areas",
     async execute({ client, message, user }) {
-        const areasEmbed = {
-            color: 0x0099ff,
-            title: "Areas 🗺️",
-            author: {
-                name: message.author.username,
-                icon_url: message.author.displayAvatarURL({ dynamic: true }),
-            },
-            description: `All the areas are listed down below. You are currently at **Area [${
-                user.location.area || 1
-            } - ${user.location.floor}]**.`,
-            thumbnail: {
-                url: client.user.displayAvatarURL({ dynamic: true }),
-            },
-            fields: [],
-            footer: {
-                text: "Page 1 | Areas: 3 / 10",
-            },
-        };
-
+        const areas = [];
+        const areaArray = [];
         for (const id in AreaInterface.areas) {
             const area = AreaInterface.areas[id];
-            areasEmbed.fields.push({
+            areaArray.push({
                 name: `${area.getID}. ${area.getName}`,
                 value: `${area.getDesc}`,
             });
         }
 
-        message.channel.send({ embeds: [areasEmbed] });
+        const itemsPerPage = 10;
+        const chunks = splitArrayIntoChunksOfLen(areaArray, itemsPerPage);
+        chunks.forEach((chunk, index) => {
+            const embed = new MessageEmbed()
+                .setAuthor({
+                    name: message.author.username,
+                    icon_url: message.author.displayAvatarURL({
+                        dynamic: true,
+                    }),
+                })
+                .setColor(client.config.colors.primary)
+                .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+                .setTitle("Areas 🗺️")
+                .setDescription(
+                    `All the areas are listed down below. You are currently at **Area [${
+                        user.location.area || 1
+                    } - ${user.location.floor || 1}]**.`,
+                )
+                .addFields(chunk)
+                .setFooter({
+                    text: `Page ${index + 1} | Areas: ${
+                        chunk.length + index * itemsPerPage
+                    }/${areaArray.length}`,
+                });
+
+            areas.push(embed);
+        });
+
+        const paginated = new PaginateContent(client, message, areas);
+        paginated.init();
     },
 };
+
+function splitArrayIntoChunksOfLen(arr, len) {
+    const chunks = [];
+    const n = arr.length;
+    let i = 0;
+
+    while (i < n) {
+        chunks.push(arr.slice(i, (i += len)));
+    }
+    return chunks;
+}
