44a3e2d55fe8dda7c4f0b41d8f716de0cea44928
finder <0xfinder@protonmail.com>
2022-03-23 22:36:30 +0800

    fix undef bug

--- src/commands/areas/areas.js
+++ src/commands/areas/areas.js
@@ -1,25 +1,26 @@
-const AreaInterface = require('./AreaInterface.js');
+const AreaInterface = require("./AreaInterface.js");
 
 module.exports = {
-    name: 'areas',
-    aliases: [''],
+    name: "areas",
+    aliases: [""],
     category: "Areas",
     async execute({ client, message, user }) {
-
         const areasEmbed = {
             color: 0x0099ff,
-            title: 'Areas 🗺️',
+            title: "Areas 🗺️",
             author: {
                 name: message.author.username,
                 icon_url: message.author.displayAvatarURL({ dynamic: true }),
             },
-            description: `All the areas are listed down below. You are currently at **Area [${user.location.area} - ${user.location.floor}]**.`,
+            description: `All the areas are listed down below. You are currently at **Area [${
+                user.location.area || 1
+            } - ${user.location.floor}]**.`,
             thumbnail: {
                 url: client.user.displayAvatarURL({ dynamic: true }),
             },
             fields: [],
             footer: {
-                text: 'Page 1 | Areas: 3 / 10',
+                text: "Page 1 | Areas: 3 / 10",
             },
         };
 
