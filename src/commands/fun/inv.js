b89ff7508cf2615723f943dd4e6e649416e22992
finder <0xfinder@protonmail.com>
2022-03-30 15:10:55 +0800

    shift stuff

--- src/commands/fun/inv.js
+++ src/commands/fun/inv.js
@@ -1,6 +1,9 @@
 const { MessageEmbed } = require("discord.js");
 const invUtil = require("./utils/invUtil.js");
-const { PaginateContent } = require("../../functions/pagination/Pagination");
+const {
+    PaginateContent,
+    splitArrayIntoChunksOfLen,
+} = require("../../functions/pagination/Pagination");
 
 module.exports = {
     name: "inventory",
@@ -63,14 +66,3 @@ module.exports = {
         paginated.init();
     },
 };
-
-function splitArrayIntoChunksOfLen(arr, len) {
-    const chunks = [];
-    const n = arr.length;
-    let i = 0;
-
-    while (i < n) {
-        chunks.push(arr.slice(i, (i += len)));
-    }
-    return chunks;
-}
