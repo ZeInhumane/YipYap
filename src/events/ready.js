0ed795eef8afee4189f16b6359d8a6b09740b4ac
finder <0xfinder@protonmail.com>
2022-04-01 23:54:02 +0800

    refactor: clean code

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,14 +5,29 @@ const mongoose = require('mongoose');
 const BotData = require('../models/botData');
 
 
-module.exports = async client => {
+module.exports = async (client) => {
+    // Set the bot's presence
+    setPresence(client);
+
+    // Set cooldowns
+    setCooldowns();
+
+    console.log(`${client.user.username} has been restarted.`);
+
+    console.log("Loading slash commands");
+    require('../handlers/slash')(client);
+};
+
+const setPresence = (client) => {
     client.user.setPresence({
         activities: [
             { name: `${client.guilds.cache.size.toLocaleString()} servers| -help for help`, type: "PLAYING" },
         ],
         status: "online",
     });
+};
 
+const setCooldowns = () => {
     let cooldowns;
     BotData.findOne({ dataName: 'Cooldowns' }, (err, Data) => {
         if (err) console.log(err);
@@ -36,6 +51,4 @@ module.exports = async client => {
             });
         }, 120000);
     });
-
-    console.log(`${client.user.username} has been restarted.`);
 };
