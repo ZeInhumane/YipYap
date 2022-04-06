aa28be00171c82cf63345859cbd5848d1e3f8fd9
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2022-04-06 23:18:24 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
- apps : [{
-    script    : "index.js",
-    instances : "max",
-    exec_mode : "cluster",
+  apps: [{
+    script: "index.js",
+    instances: "max",
+    exec_mode: "cluster",
   }],
   "scripts": {
     "start": "pm2-runtime start index.js -i 0",
   },
+  env_production: {
+    NODE_ENV: "production",
+  }
 };
